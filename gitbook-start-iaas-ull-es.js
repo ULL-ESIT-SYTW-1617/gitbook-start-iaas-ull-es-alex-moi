a871dde3516fd9c09c1e48715e767f1962a41256
alu0100767421 <alu0100767421@ull.edu.es>
2016-10-20 19:57:20 +0000

    funcionalidades del deploy

--- gitbook-start-iaas-ull-es.js
+++ gitbook-start-iaas-ull-es.js
@@ -1,6 +1,11 @@
 
 var exec = require("ssh-exec");
 var fs = require('fs');
+var path = require('path');
+
+const GitUrlParse = require("git-url-parse");
+
+
 
 
 
@@ -10,11 +15,56 @@ function initialize() {
 };
 
 function deploy(ip, ruta, url) {
+
+    var carpeta = GitUrlParse(url);
+
     console.log("\n Comenzando el deploy en Iaas");
     console.log('Direccion IP Destino: '+ip);
     console.log('Ruta de destino: '+ruta);
     console.log('Repositorio origen: '+url);
+    console.log( carpeta.name);
+
+   /* fs.existsSync(path.resolve(ruta, carpeta.name)) ? pull(): clone();
+	console.log(fs.existsSync(path.resolve(ruta+carpeta.name, carpeta.name)));
+    console.log(path.resolve(__dirname));
+
+var pepe = exec('if [ -d "$'+ruta+'/'+carpeta.name+'/$i" ]; then echo "true" > '+ruta+'+'/'+'+carpeta.name+'/yes.txt else echo "false"; fi',{
+          user: 'usuario',
+          host: ip,
+          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
     
+    },function(err){
+     if(err)
+    	console.log('boool errorrrrr');
+    });
+
+//console.log(pepe);
+
+function clone(){
+	console.log("esta entrando en clone");
+   	 exec('cd '+ruta+';git clone '+url+'',{
+          user: 'usuario',
+          host: ip,
+          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
+	 },function(err){
+	     if(err)
+	    	console.log('Ha habido un error');
+	    });
+  	}
+  
+  function pull(){
+console.log("esta entrando en pull");
+    exec('cd '+ruta+'/'+carpeta.name+';git pull',{
+          user: 'usuario',
+          host: ip,
+          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
+    
+    },function(err){
+     if(err)
+    	console.log('Ha habido un error2');
+    });
+}
+*/
 
     exec('cd '+ruta+';git clone '+url+'',{
           user: 'usuario',
