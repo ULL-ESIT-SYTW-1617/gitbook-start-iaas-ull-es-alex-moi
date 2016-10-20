07e00ac9f6a5db2eba0da38efa8f085cb373548a
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-20 10:57:52 +0000

    Subiendo cambios a ejecutable

--- gitbook-start-iaas-ull-es.js
+++ gitbook-start-iaas-ull-es.js
@@ -1,7 +1,22 @@
 
 
+/*var config = {
+  host: '10.6.128.129',
+  port: 22,
+  username: 'usuario',
+  privateKey: fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)
+}
+ 
+var gulpSSH = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: config
+})
+*/
+  /*return gulpSSH
+    .shell(['cd /home/usuario/src/sytw/iaas', 'git pull']);*/
+
 var deploy = function(ip, ruta) {
-    console.log("Funcionando");
+    console.log("\nFuncionando");
     console.log(ip)
     console.log(ruta)
 };
