96f02edf18cc63df2e9d9007132bf89002413166
alu0100767421 <alu0100767421@ull.edu.es>
2016-10-23 11:00:31 +0000

    Solucionando error de actualizacion en la maquina iaas

--- gitbook-start-iaas-ull-es.js
+++ gitbook-start-iaas-ull-es.js
@@ -2,7 +2,6 @@
 var exec = require("ssh-exec");
 var fs = require('fs');
 var path = require('path');
-
 const GitUrlParse = require("git-url-parse");
 
 
@@ -47,64 +46,36 @@ function deploy(ip, ruta, url) {
 
     var carpeta = GitUrlParse(url);
 
-    console.log("\n Comenzando el deploy en Iaas");
+    console.log("Comenzando el deploy en Iaas");
     console.log('Direccion IP Destino: '+ip);
-    console.log('Ruta de destino: '+ruta);
+    console.log('Ruta de destino: '+ruta+'/'+carpeta.name);
     console.log('Repositorio origen: '+url);
-    console.log( carpeta.name);
-
-   /* fs.existsSync(path.resolve(ruta, carpeta.name)) ? pull(): clone();
-	console.log(fs.existsSync(path.resolve(ruta+carpeta.name, carpeta.name)));
-    console.log(path.resolve(__dirname));
-
-var pepe = exec('if [ -d "$'+ruta+'/'+carpeta.name+'/$i" ]; then echo "true" > '+ruta+'+'/'+'+carpeta.name+'/yes.txt else echo "false"; fi',{
-          user: 'usuario',
-          host: ip,
-          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
     
-    },function(err){
-     if(err)
-    	console.log('boool errorrrrr');
-    });
 
-//console.log(pepe);
-
-function clone(){
-	console.log("esta entrando en clone");
-   	 exec('cd '+ruta+';git clone '+url+'',{
-          user: 'usuario',
-          host: ip,
-          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
-	 },function(err){
-	     if(err)
-	    	console.log('Ha habido un error');
-	    });
-  	}
-  
-  function pull(){
-console.log("esta entrando en pull");
-    exec('cd '+ruta+'/'+carpeta.name+';git pull',{
-          user: 'usuario',
-          host: ip,
-          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
-    
-    },function(err){
-     if(err)
-    	console.log('Ha habido un error2');
-    });
-}
-*/
 
     exec('cd '+ruta+';git clone '+url+'',{
           user: 'usuario',
           host: ip,
           key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
     
-    },function(err){
-     if(err)
-    	console.log('Ha habido un error');
+      },function(err){
+       if(err){
+      	console.log('Haciendo pull del repositorio!');
+        exec('cd '+ruta+'/'+carpeta.name+'; git pull',{
+            user: 'usuario',
+            host: ip,
+            key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
+          },function(err){ 
+            if(err)
+                console.log("Ha habido un error con el pull");
+            else
+                console.log("Actualizacion carpeta confirmada");
+            });
+        }
+        else {
+            console.log("Clonación del repositorio confirmada");
+        }
     });
-
 };
 
 module.exports = {
